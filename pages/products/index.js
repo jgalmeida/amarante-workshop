69f49aee7787794638915aa47223a063f5f343c4
claudia.conceicao <claudia.conceicao@farfetch.com>
2021-10-23 20:52:37 +0100

    feat(FE): Create product page

--- pages/products/index.js
+++ pages/products/index.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import Layout from '../../components/layout'
 import Head from 'next/head'
 import Section from '../../components/section'
-import utilStyles from '../../styles/utils.module.css'
 import styles from './index.module.css'
 import Image from 'next/image'
 
---
+++ pages/products/product.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Layout from '../../components/layout'
+import Head from 'next/head'
+import Section from '../../components/section'
+import styles from './product.module.css'
+import Image from 'next/image'
+
+export default function Product() {
+  return (
+    <Layout>
+      <Head>
+        <title>Product</title>
+      </Head>
+
+      <Section title="Produto">
+      </Section>
+    </Layout>
+  )
+}
